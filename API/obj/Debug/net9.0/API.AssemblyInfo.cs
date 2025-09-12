615026f02ced8f798993f72e9214a1cfc9761206
ABDUL SUBHAN <ABDULSUBHAN2@BAGH.MTBC.COM>
2025-09-12 15:32:44 +0500

    Updated Code

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bfae25dffdd8d20a35f2b3bf5ba2ff14837b613")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+218be444a8e518d16234f7e254ea351b4a815727")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
